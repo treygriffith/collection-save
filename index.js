411a24b406a267fc4cf3f786ed5a220857c8f35c
Trey Griffith <trey.griffith@gmail.com>
2013-09-10 23:25:43 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+/**
+ * Module dependencies.
+ */
+var buffer = require('buffer');
+
+/**
+ * Plugin.
+ */
+
+module.exports = function (Collection) {
+
+  Collection.on('construct', function (collection) {
+
+      collection.model.on('saving', function () {
+        if (collection.options.updateChangedModels !== false) collection.updateChangedModels();
+        if (collection.options.saveNewModels !== false) collection.saveNewModels();
+      });
+
+      if (options.saveOnPlaceholder) {
+        collection.on('placeholder', buffer(function () {
+          collection.model.save();
+        }, 100));
+      }
+  });
+};
